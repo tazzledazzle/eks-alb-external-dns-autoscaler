02e9da20101f6f3b61bb8dc207e13d8690431008
Terence Schumacher <terenceschumacher@gmail.com>
2025-05-22 13:00:54 -0700

    Update versions.tf; reformat

--- versions.tf
+++ versions.tf
@@ -2,9 +2,18 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-    aws        = { source = "hashicorp/aws",        version = "~> 5.0" }
-    helm       = { source = "hashicorp/helm",       version = "~> 2.13" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.25" }
+    aws = { 
+      source = "hashicorp/aws",
+      version = "~> 5.0" 
+    }
+    helm = {
+      source = "hashicorp/helm",
+      version = "~> 2.13"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes",
+      version = "~> 2.25"
+    }
   }
 }
 
